5cc2eac71eb51384074eff8effbbf123df9bbc84
Kaabislam <islamkaab13@gmail.com>
2024-10-11 14:06:44 +0600

    Project creation

---
+++ src/main/java/com/kaab/repair_service/RepairServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kaab.repair_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairServiceApplication.class, args);
+	}
+
+}
